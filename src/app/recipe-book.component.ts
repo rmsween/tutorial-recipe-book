c68bffbb34eee173d779b8bf9adf1f930c423614
Sweeney <ryan@squarecrop.com>
2017-02-16 12:33:32 -0500

    3.36 Creating the Header Component

---
+++ src/app/recipe-book.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+import { HeaderComponent } from './header.component';
+
+@Component({
+  selector: 'rb-root',
+  templateUrl: './recipe-book.component.html',
+})
+export class RecipeBookComponent {
+  title = 'rb works!';
+}
