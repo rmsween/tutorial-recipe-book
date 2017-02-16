b4c301b96d32be3a3567d3573ea131f7c42f66c9
Sweeney <ryan@squarecrop.com>
2017-02-16 14:02:40 -0500

    3.39 Recipe Detail and Databinding

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 
 import { Recipe } from '../recipe';
 import { RecipeItemComponent } from './recipe-item.component';
@@ -10,11 +10,16 @@ import { RecipeItemComponent } from './recipe-item.component';
 })
 export class RecipeListComponent implements OnInit {
   recipes: Recipe[] = [];
+  @Output() recipeSelected = new EventEmitter<Recipe>();
   recipe = new Recipe('Dummy', 'Dummy', 'http://www.polyvore.com/cgi/img-thing?.out=jpg&size=l&tid=51790881');
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onSelected(recipe: Recipe) {
+    this.recipeSelected.emit(recipe);
   }
 
 }
--- src/app/recipes/recipes.component.ts
+++ src/app/recipes/recipes.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Recipe } from './recipe';
 import { RecipeListComponent } from './recipe-list/recipe-list.component';
+import { RecipeDetailComponent } from './recipe-detail/recipe-detail.component';
 
 @Component({
   moduleId: module.id,
@@ -8,6 +10,8 @@ import { RecipeListComponent } from './recipe-list/recipe-list.component';
   templateUrl: './recipes.component.html'
 })
 export class RecipesComponent implements OnInit {
+
+  selectedRecipe: Recipe;
 
   constructor() { }
 
