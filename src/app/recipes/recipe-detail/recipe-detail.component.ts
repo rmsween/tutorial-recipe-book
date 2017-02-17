f32f87a2c64bad611073dd7a28e9875a47b90341
Sweeney <ryan@squarecrop.com>
2017-02-17 16:09:53 -0500

    10.98 Extracting Recipe Data from Route Params

--- src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -1,6 +1,9 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Subscription } from 'rxjs/Rx';
 
 import { Recipe } from '../recipe';
+import { RecipeService } from '../recipe.service';
 import { ShoppingListService } from '../../shopping-list/shopping-list.service';
 
 @Component({
@@ -8,12 +11,27 @@ import { ShoppingListService } from '../../shopping-list/shopping-list.service';
   selector: 'rb-recipe-detail',
   templateUrl: './recipe-detail.component.html'
 })
-export class RecipeDetailComponent implements OnInit {
-  @Input() selectedRecipe: Recipe;
+export class RecipeDetailComponent implements OnInit, OnDestroy {
+  private subscription: Subscription;
+  private recipeIndex: number;
+  selectedRecipe: Recipe;
 
-  constructor(private sls: ShoppingListService) { }
+  constructor(private sls: ShoppingListService,
+              private route: ActivatedRoute,
+              private recipesService: RecipeService) {
+  }
 
   ngOnInit() {
+    this.subscription = this.route.params.subscribe(
+        (params: any) => {
+          this.recipeIndex = params['id'];
+          this.selectedRecipe = this.recipesService.getRecipe(this.recipeIndex);
+        }
+    );
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
   }
 
   onAddToShoppingList() {
--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -19,4 +19,8 @@ export class RecipeService {
     return this.recipes;
   }
 
+  getRecipe(id: number) {
+    return this.recipes[id];
+  }
+
 }
